28c47feb2e7e2e9d8d7c1ccdf0569eb8ae9277b3
Luis Sanz <sanz.luis0@gmail.com>
2018-07-23 15:25:45 -0400

    Deploy to gh-pages

--- _next/static/commons/manifest.js
+++ _next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "1507873244c35dab7c0f"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "5a8550c2bc6f7e41d21b"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
